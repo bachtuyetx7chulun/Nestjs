b2282d2d63147324b47c16c013a0ffd36dbd1470
Medium_Hieu <77453052+bachtuyetx7chulun@users.noreply.github.com>
2021-12-14 22:45:15 +0700

    Initial commit

---
+++ prisma/migrations/20211214155823_/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Action" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Action_pkey" PRIMARY KEY ("id")
+);
